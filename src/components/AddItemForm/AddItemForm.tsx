d36ee27d0162e7a4c71d9473816b1a0b69080665
ruslan <zrm1306@gmail.com>
2023-09-28 19:19:46 +0500

    add custom hook for form

--- src/components/AddItemForm/AddItemForm.tsx
+++ src/components/AddItemForm/AddItemForm.tsx
@@ -1,8 +1,9 @@
 import {Box, TextField} from "@mui/material";
 import Button from "@mui/material/Button";
 import AddIcon from '@mui/icons-material/Add';
-import {ChangeEvent, FC, useState} from "react";
-import React,{KeyboardEvent} from "react";
+import {FC } from "react";
+import React from "react";
+import {useAddItemForm} from "./hooks/useAddItemForm";
 
 type PropsType = {
     callback: (title: string) => void
@@ -10,32 +11,9 @@ type PropsType = {
 }
 
 export const AddItemForm: FC<PropsType> = React.memo(({callback,maxLengthValue}) => {
-    const [value, setValue] = useState('')
-    const [lengthError,setLengthError] = useState(false)
-    const [error,setError] = useState(false)
-    const handleAddItem = () => {
-        if (value.trim().length !== 0 && value.length <= maxLengthValue) {
-            callback(value)
-            setValue('')
-        } else {
-            setError(true)
-        }
-    }
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const target = e.currentTarget.value
-        if (error) {
-            setError(false)
-        }
-        if(target.length > maxLengthValue) {
-            setLengthError(true)
-        }else if(lengthError){
-            setLengthError(false)
-        }
-        setValue(target)
-    }
-    const addItemOnEnter = (e:KeyboardEvent<HTMLInputElement>) => {
-        if(e.key === 'Enter') handleAddItem()
-    }
+    const {value,handleChange,lengthError, error,
+        addItemOnEnter,handleAddItem
+    } = useAddItemForm(maxLengthValue,callback)
     return (
         <Box sx={{display: 'flex', alignItems: 'center'}}>
             <TextField
@@ -59,3 +37,54 @@ export const AddItemForm: FC<PropsType> = React.memo(({callback,maxLengthValue})
         </Box>
     )
 })
+
+// export const AddItemForm: FC<PropsType> = React.memo(({callback,maxLengthValue}) => {
+//     const [value, setValue] = useState('')
+//     const [lengthError,setLengthError] = useState(false)
+//     const [error,setError] = useState(false)
+//     const handleAddItem = () => {
+//         if (value.trim().length !== 0 && value.length <= maxLengthValue) {
+//             callback(value)
+//             setValue('')
+//         } else {
+//             setError(true)
+//         }
+//     }
+//     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+//         const target = e.currentTarget.value
+//         if (error) {
+//             setError(false)
+//         }
+//         if(target.length > maxLengthValue) {
+//             setLengthError(true)
+//         }else if(lengthError){
+//             setLengthError(false)
+//         }
+//         setValue(target)
+//     }
+//     const addItemOnEnter = (e:KeyboardEvent<HTMLInputElement>) => {
+//         if(e.key === 'Enter') handleAddItem()
+//     }
+//     return (
+//         <Box sx={{display: 'flex', alignItems: 'center'}}>
+//             <TextField
+//                 size={'small'}
+//                 id="standard-basic"
+//                 variant="outlined"
+//                 value={value}
+//                 onChange={handleChange}
+//                 sx={{padding:0}}
+//                 error={lengthError || error}
+//                 onKeyDown={addItemOnEnter}
+//             />
+//             <Button
+//                 onClick={handleAddItem}
+//                 size={"small"}
+//                 disableElevation
+//                 variant={'contained'}
+//                 sx={{padding: 1}}
+//             ><AddIcon fontSize={"small"}/>
+//             </Button>
+//         </Box>
+//     )
+// })
