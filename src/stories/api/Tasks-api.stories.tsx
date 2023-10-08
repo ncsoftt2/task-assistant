ab78db059d3c062422190fa371ef9b879f02a79b
ruslan <zrm1306@gmail.com>
2023-10-08 10:49:57 +0500

    add api stories for tasks/todolists

---
+++ src/stories/api/Tasks-api.stories.tsx
@@ -0,0 +1,89 @@
+import {ChangeEvent, useState} from "react";
+import {TodoListApiType} from "../../api/todolist-api";
+import {tasksAPI, TaskType} from "../../api/tasks-api";
+
+export default {
+    title: 'API/Tasks'
+}
+
+export const GetTasks = () => {
+    const [state, setState] = useState<TodoListApiType[]>([])
+    const [todoId, setTodoId] = useState<string>('')
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => setTodoId(e.currentTarget.value)
+    const handleClick = () => {
+        tasksAPI.getTasks(todoId)
+            .then(res => setState(res.data.items))
+    }
+    return (
+        <div>
+            <input placeholder={'todo id'} value={todoId} onChange={handleChange}/>
+            <button onClick={handleClick}>get</button>
+            {state.map(el => {
+                return (
+                    <div style={{display: 'flex'}}>
+                        <div style={{marginRight: '20px'}}>todoId: {el.id}</div>
+                        <div>title: {el.title}</div>
+                    </div>
+                )
+            })
+            }
+        </div>
+    )
+}
+export const CreateTask = () => {
+    const [state, setState] = useState<any>(null)
+    const [todoId, setTodoId] = useState<string>('')
+    const [title,setTitle] = useState<string>('')
+    const handleChangeTodoId = (e: ChangeEvent<HTMLInputElement>) => setTodoId(e.currentTarget.value)
+    const handleChangeTitle = (e: ChangeEvent<HTMLInputElement>) => setTitle(e.currentTarget.value)
+    const handleClick = () => {
+        tasksAPI.createTask(todoId,title)
+            .then(res => setState(res.data.data.item))
+    }
+    return (
+        <div>
+            <input placeholder={'todo id'} value={todoId} onChange={handleChangeTodoId}/>
+            <input placeholder={'task title'} value={title} onChange={handleChangeTitle}/>
+            <button onClick={handleClick}>create</button>
+        </div>
+    )
+}
+export const DeleteTask = () => {
+    const [state, setState] = useState<any>(null)
+    const [todoId,setTodoId] = useState<string>('')
+    const [taskId,setTaskId] = useState<string>('')
+    const handleChangeTodoId = (e:ChangeEvent<HTMLInputElement>) => setTodoId(e.currentTarget.value)
+    const handleChangeTaskId = (e:ChangeEvent<HTMLInputElement>) => setTaskId(e.currentTarget.value)
+    const handleClick = () => {
+        tasksAPI.deleteTask(todoId,taskId)
+            .then(res => setState(res.data))
+    }
+    return (
+        <div>
+            <input placeholder={'todo id'} value={todoId} onChange={handleChangeTodoId}/>
+            <input placeholder={'task id'} value={taskId} onChange={handleChangeTaskId}/>
+            <button onClick={handleClick}>delete</button>
+        </div>
+    )
+}
+export const UpdateTaskTitle = () => {
+    const [state, setState] = useState<any>(null)
+    const [todoId,setTodoId] = useState<string>('')
+    const [taskId,setTaskId] = useState<string>('')
+    const [title,setTitle] = useState<string>('')
+    const handleChangeTodoId = (e:ChangeEvent<HTMLInputElement>) => setTodoId(e.currentTarget.value)
+    const handleChangeTaskId = (e:ChangeEvent<HTMLInputElement>) => setTaskId(e.currentTarget.value)
+    const handleChangeTaskTitle = (e:ChangeEvent<HTMLInputElement>) => setTitle(e.currentTarget.value)
+    const handleClick = () => {
+        tasksAPI.updateTask(todoId,taskId,title)
+            .then(res => setState(res.data))
+    }
+    return (
+        <div>
+            <input placeholder={'todo id'} value={todoId} onChange={handleChangeTodoId}/>
+            <input placeholder={'task id'} value={taskId} onChange={handleChangeTaskId}/>
+            <input placeholder={'task title'} value={title} onChange={handleChangeTaskTitle}/>
+            <button onClick={handleClick}>update</button>
+        </div>
+    )
+}
