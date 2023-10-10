81b61887ddc6c805ff43ac48e8b0c8e733812c96
ruslan <zrm1306@gmail.com>
2023-10-10 18:11:20 +0500

    update commit: add middleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,9 @@
-import {combineReducers, compose, legacy_createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, legacy_createStore} from "redux";
 import {todoReducer} from "./reducers/todos-reducer/todo-reducer";
 import {taskReducer} from "./reducers/task-reducer/task-reducer";
+import thunk, {ThunkAction, ThunkDispatch} from "redux-thunk";
+import {TodoActionType} from "./reducers/todos-reducer/todo-actions";
+import {TaskActionType} from "./reducers/task-reducer/task-actions";
 
 declare global {
     interface Window {
@@ -16,8 +19,9 @@ export const reducers = combineReducers({
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 export const store = legacy_createStore(
     reducers,
-    composeEnhancers()
+    composeEnhancers(applyMiddleware(thunk))
 )
-
+type Actions = TodoActionType | TaskActionType
 export type AppState = ReturnType<typeof reducers>
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = ThunkDispatch<AppState, unknown, Actions>
+export type ThunkType = ThunkAction<void, AppState, unknown, Actions>
