220d2d17a548db46bb36fdb4443f472fd556500f
ruslan <zrm1306@gmail.com>
2023-11-04 11:28:35 +0500

    add custom hook for login form

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -7,47 +7,17 @@ import FormGroup from '@mui/material/FormGroup';
 import FormLabel from '@mui/material/FormLabel';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import {useFormik} from "formik";
-import {useAppDispatch, useAppSelector} from "../../store/hooks";
+import {useAppSelector} from "../../store/hooks";
 import {Navigate} from "react-router-dom";
 import {routes} from "../../routes/routes";
-import { loginTC } from './auth-reducer';
+import {useLogin} from "./useLogin";
 
-type FormikErrorType = {
-    email?: string
-    password?: string
-    rememberMe?: boolean
-}
+
 
 export const Login = () => {
     const {isAuth} = useAppSelector(({auth}) => auth)
-    const dispatch = useAppDispatch()
-    const formik = useFormik({
-        initialValues: {
-            email: '',
-            password: '',
-            rememberMe: false
-        },
-        validate: (values) => {
-            const errors: FormikErrorType = {}
-            if (!values.email) {
-                errors.email = 'Обязательное свойство'
-            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-                errors.email = 'Неправильный email'
-            }
-            if(!values.password) {
-                errors.password = 'Обязательное свойство'
-            } else if (values.password.trim().length < 4) {
-                errors.password = 'Минимум 4 символа'
-            }
-            return errors
-        },
-        onSubmit: values => {
-            dispatch(loginTC(values))
-        }
-    })
+    const {formik} = useLogin()
     if(isAuth) return <Navigate to={routes.main}/>
-
     return <Grid container justifyContent={'center'}>
         <Grid item justifyContent={'center'}>
             <form onSubmit={formik.handleSubmit}>
@@ -59,25 +29,25 @@ export const Login = () => {
                     <FormGroup>
                         <TextField label="Email"
                                    margin="normal"
-                                   name="email"
-                                   value={formik.values.email}
-                                   onChange={formik.handleChange}
-                                   onBlur={formik.handleBlur}
+                                   helperText={formik.touched.email && formik.errors.email}
+                                   {...formik.getFieldProps('email')}
                         />
                         <TextField type="password"
                                    label="Password"
                                    margin="normal"
-                                   name="password"
-                                   value={formik.values.password}
-                                   onChange={formik.handleChange}
-                                   onBlur={formik.handleBlur}
+                                   helperText={formik.touched.password && formik.errors.password}
+                                   {...formik.getFieldProps('password')}
                         />
                         <FormControlLabel label={'Remember me'} control={<Checkbox
                             name="rememberMe"
                             checked={formik.values.rememberMe}
                             onChange={formik.handleChange}
                         />}/>
-                        <Button type={'submit'} variant={'contained'} color={'primary'}>
+                        <Button
+                            disabled={formik.isSubmitting}
+                            type={'submit'}
+                            variant={'contained'}
+                            color={'primary'}>
                             Login
                         </Button>
                     </FormGroup>
---
+++ src/features/Login/useLogin.ts
@@ -0,0 +1,40 @@
+import {useAppDispatch} from "../../store/hooks";
+import {useFormik} from "formik";
+import {loginTC} from "./auth-reducer";
+
+type FormikErrorType = {
+    email?: string
+    password?: string
+    rememberMe?: boolean
+}
+
+export const useLogin = () => {
+    const dispatch = useAppDispatch()
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            rememberMe: false
+        },
+        validate: (values) => {
+            const errors: FormikErrorType = {}
+            if (!values.email) {
+                errors.email = 'Обязательное свойство'
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Неправильный email'
+            }
+            if(!values.password) {
+                errors.password = 'Обязательное свойство'
+            } else if (values.password.trim().length < 4) {
+                errors.password = 'Минимум 4 символа'
+            }
+            return errors
+        },
+        onSubmit: (values,{setSubmitting}) => {
+            setSubmitting(true)
+            dispatch(loginTC(values))
+            setSubmitting(false)
+        }
+    })
+    return {formik}
+}
