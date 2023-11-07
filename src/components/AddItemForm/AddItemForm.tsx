b2fedda9f84e51ba19187b0da532babb1954a157
ruslan <zrm1306@gmail.com>
2023-11-07 15:36:42 +0500

    change redux-thunk on createAsyncThunk

--- src/components/AddItemForm/AddItemForm.tsx
+++ src/components/AddItemForm/AddItemForm.tsx
@@ -1,24 +1,17 @@
 import {FC, memo} from "react";
-import { Box, makeStyles, TextField } from "@mui/material";
+import { Box, TextField } from "@mui/material";
 import Button from "@mui/material/Button";
 import AddIcon from '@mui/icons-material/Add';
 import { useAddItemForm } from "./hooks/useAddItemForm";
-import {useAppSelector} from "../../store/hooks";
-
-
-
-
-
 export type FormPropsType = {
     callback: (title:string) => void
     disabled?: boolean
 }
 export const AddItemForm:FC<FormPropsType> = memo(({callback,disabled = false}) => {
-    const status = useAppSelector(state => state.app.status)
     const {
         value, handleChange, error,
         addItemOnEnter, handleAddItem
-    } = useAddItemForm(callback,status)
+    } = useAddItemForm(callback)
     return (
         <Box sx={{display: 'flex', alignItems: 'center',justifyContent:'center'}}>
             <TextField
