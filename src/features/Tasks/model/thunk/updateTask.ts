5419629a6b195a7ae1bb42b475107a3f8b0c1c0d
ruslan <zrm1306@gmail.com>
2023-11-20 12:51:46 +0500

    unnecessary imports updateTask removed

--- src/features/Tasks/model/thunk/updateTask.ts
+++ src/features/Tasks/model/thunk/updateTask.ts
@@ -1,13 +1,10 @@
-import {AppState} from "app/model/store";
 import {setAppStatusAC} from "app/model/slice/app-reducer";
-import {createAsyncThunk} from "@reduxjs/toolkit";
 import {UpdateTaskModelType} from "features/Tasks/api/taskApi.types";
-import {FieldsErrorsType} from "common/types";
 import {tasksAPI} from "features/Tasks/api/taskApi";
 import {handleNetworkError, handleServerError} from "common/utils";
-import {taskActions} from "features/Tasks/index";
 import {ResultCode} from "common/enums";
 import {createAppAsyncThunk} from "common/utils/createAsyncThunkApp";
+import { tasksActionsCreators } from "../slice/taskSlice";
 
 type ReturnValuesType = { todoId: string, taskId: string, model: UpdateTaskModelType };
 export const updateTaskTC = createAppAsyncThunk<
@@ -17,7 +14,7 @@ export const updateTaskTC = createAppAsyncThunk<
     'task/updateTask',
     async ({taskId,todoId,model},{getState,dispatch,rejectWithValue}) => {
         dispatch(setAppStatusAC({status:'loading'}))
-        dispatch(taskActions.changeTaskStatusAC({todoId,taskId,taskStatus:'loading'}))
+        dispatch(tasksActionsCreators.changeTaskStatusAC({todoId,taskId,taskStatus:'loading'}))
         try {
             const state = getState()
             const task = state.tasks[todoId].find(t => t.id === taskId)
@@ -26,18 +23,18 @@ export const updateTaskTC = createAppAsyncThunk<
                 const response = await tasksAPI.updateTask(todoId,taskId,apiModel)
                 if(response.data.resultCode === ResultCode.SUCCESS) {
                     dispatch(setAppStatusAC({status:'succeeded'}))
-                    dispatch(taskActions.changeTaskStatusAC({todoId,taskId,taskStatus:'succeeded'}))
+                    dispatch(tasksActionsCreators.changeTaskStatusAC({todoId,taskId,taskStatus:'succeeded'}))
                     return {taskId,todoId, model:response.data.data.item}
                 } else {
                     handleServerError(response.data,dispatch)
-                    dispatch(taskActions.changeTaskStatusAC({todoId,taskId,taskStatus:'failed'}))
+                    dispatch(tasksActionsCreators.changeTaskStatusAC({todoId,taskId,taskStatus:'failed'}))
                     return rejectWithValue(null)
                 }
             }
         } catch (e) {
             const err = e as {message:string}
             handleNetworkError(err,dispatch)
-            dispatch(taskActions.changeTaskStatusAC({todoId,taskId,taskStatus:'failed'}))
+            dispatch(tasksActionsCreators.changeTaskStatusAC({todoId,taskId,taskStatus:'failed'}))
             return rejectWithValue(null)
         }
     }
