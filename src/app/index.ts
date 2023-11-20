3139b00019b67907304e9f4823f90a171c8ae74a
ruslan <zrm1306@gmail.com>
2023-11-20 12:52:11 +0500

    unnecessary imports app/index removed

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,14 +1,12 @@
-import { AppRoutes } from './model/routes/AppRoutes'
+import { AppRouter } from 'app/providers/router/ui/AppRouter'
 import * as appSelectors from './model/selectors/appSelectors'
 import {appReducer, slice} from './model/slice/app-reducer'
-import { App } from './ui/App'
 import {initializedTC} from "./model/thunk/me";
 
 const appActions = {initializedTC,...slice.actions}
 export {
     appSelectors,
     appReducer,
-    AppRoutes,
-    App,
+    AppRouter,
     appActions
 }
